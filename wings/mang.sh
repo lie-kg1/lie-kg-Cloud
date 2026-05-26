fdbb6c4f3465476eb69d99705cc72257adfe11bd
lie_kg <fetgeryrd0187@gmail.com>
2026-05-26 17:52:49 +0300

    Update mang.sh

--- wings/mang.sh
+++ wings/mang.sh
@@ -282,7 +282,7 @@ auto_setup() {
             sleep 1
             
             # Run Config Script
-            bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/Nobita-Cloud/refs/heads/main/wings/config.sh)
+            bash <(curl -fsSL https://raw.githubusercontent.com/lie-kg1/lie-kg-Cloud/refs/heads/main/wings/config.sh)
             echo ""
             echo -e "${GREEN}>> Setup Complete! Press Enter to return to menu.${NC}"
             read
@@ -310,7 +310,7 @@ auto_setup() {
                 systemctl restart wings
                 
                 echo -e "${GREEN}>> Fetching Manager...${NC}"
-                bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/wings/Manag)
+                bash <(curl -fsSL https://raw.githubusercontent.com/lie-kg1/ptero/refs/heads/main/ptero/wings/Manag)
             fi
             
             echo ""
